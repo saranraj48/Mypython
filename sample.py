73e441273a241468744e54f010c4af1c07871179
saranraj48 <saranraj48@users.noreply.github.com>
2016-08-19 16:27:57 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,40 @@
+#print the string
+print "hello world"
+print "github"
+
+#basic calculation 
+
+a = 10 
+b = 20
+c = a + b
+
+print a
+
+print b 
+
+print c
+
+#create the list of the datas
+
+list = ["s","a","r","a","n"]
+
+print list
+
+#Add the data to list
+
+list.append("kumar")
+
+print list
+
+#Extended the list
+
+list.extend("sarvini")
+
+print list
+
+#create the tuples
+
+tuple = (1,2,3,4,5)
+
+print tuple
+
